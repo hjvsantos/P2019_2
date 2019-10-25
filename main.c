9dfe4d0b933f88cf9eaf854cf574b7ca64841b29
hjvsantos <joao__lucas_santos@hotmail.com>
2019-10-25 19:20:10 -0300

    C.2
    Criação da função Menu e executar. Estruturação da chamada dos botões e criação da função que irá chamar a lista que será criada

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
+#include <locale.h>
 
 struct Usuario{
 int idade;
@@ -7,8 +9,49 @@ char Nome[100];
 char Telefone[10];
 };
 
+void criarLista(){
+
+}
+
+void executar(char tecla){
+  if(tecla == '1'){
+   //inserir();
+  }else if(tecla == '2'){
+   //  pesquisar();
+  }else if(tecla=='3'){
+   //   remover();
+  }else if(tecla=='4'){
+   //  listar();
+  }else{
+   exit(0);
+  }
+}
+
+ void CriaMenu(){
+  char tecla;
+    printf("1 - Para Inserir um Aluno;\n");
+    printf("2 - Para Pesquisar um Aluno\n");
+    printf("3 - Para Remover um Aluno;\n");
+    printf("4 - Para Mostrar todos os Alunos;\n");
+    printf("O que deseja fazer?\n");
+    scanf("%c",&tecla);
+  if(tecla = "1"){
+    executar(tecla);
+  }else if(tecla = "2"){
+    executar(tecla);
+  }else if(tecla = "3"){
+    executar(tecla);
+  }else if(tecla = "4"){
+    executar(tecla);
+  }else{
+    exit(0);
+  }
+
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    setlocale(LC_ALL, "Portuguese");
+    CriaMenu();
     return 0;
 }
