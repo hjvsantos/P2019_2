2a568af2587a17ea94fc5e29f1523d50d615205c
hjvsantos <joao__lucas_santos@hotmail.com>
2019-11-01 19:48:13 -0300

    Modelo de criação de lista
    Add uma função que servirá de base para nossa criação de lista

--- main.c
+++ main.c
@@ -31,6 +31,51 @@ void executar(char tecla){
   }
 }
 
+
+//APENAS UM EXEMPLO DE CRIAÇÃO DE LISTA, PRECISAMOS ADAPTAR PARA NOSSA VERSÃO
+void criarLista(int n)
+{
+    struct node *fnNode, *tmp;
+    int num, i;
+    stnode = (struct node *)malloc(sizeof(struct node));
+
+    if(stnode == NULL) //verifica se o fnnode é NULL e se não houver alocação de memória
+    {
+        printf(" Impossível alocar memória");
+    }
+    else
+    {
+// Leitura
+
+        printf(" Input data for node 1 : ");
+        scanf("%d", &num);
+        stnode->num = num;      
+        stnode->nextptr = NULL; // links the address field to NULL
+        tmp = stnode;
+// Creating n nodes and adding to linked list
+        for(i=2; i<=n; i++)
+        {
+            fnNode = (struct node *)malloc(sizeof(struct node));
+            if(fnNode == NULL)
+            {
+                printf(" Memory can not be allocated.");
+                break;
+            }
+            else
+            {
+                printf(" Input data for node %d : ", i);
+                scanf(" %d", &num);
+ 
+                fnNode->num = num;      // links the num field of fnNode with num
+                fnNode->nextptr = NULL; // links the address field of fnNode with NULL
+ 
+                tmp->nextptr = fnNode; // links previous node i.e. tmp to the fnNode
+                tmp = tmp->nextptr; 
+            }
+        }
+    }
+}
+
 node inserir(){
 node *novoNo = NULL;
 
