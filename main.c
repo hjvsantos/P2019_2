28ce31dd682d255cc102a73df03bb8a3a22b863c
hjvsantos <joao__lucas_santos@hotmail.com>
2019-11-04 19:37:48 -0300

    update da versao
    Continuação do desenvolvimento

--- main.c
+++ main.c
@@ -1,153 +1,264 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <malloc.h>
-#include <locale.h>
+typedef struct Temp
+  {
+    int matricula;
+    char nome[50];
+    char curso[50];
+    char datnasc[30];
+    struct Temp *prox;
+  }lista;
+  // cria o inicio da lista
+  lista *inicio;
+//--------------------------------------------------------
 
-//Aqui � a estrutura do Aluno
-typedef struct{
-int idade;
-char Nome[100];
-char Telefone[10];
-}Aluno;
-
-// Aqui � a estrutura da base
-typedef struct{
-    Aluno data;
-    struct node *pLink;
-}node;
-
-// Aqui � para chamar a a��o desejada pelo usu�rio
-void executar(char tecla){
-  if(tecla == '1'){
-   //inserir();
-  }else if(tecla == '2'){
-   //pesquisar();
-  }else if(tecla=='3'){
-   //remover();
-  }else if(tecla=='4'){
-   //listar();
-  }else{
-   exit(0);
-  }
+// Funcao que define a lista como vazia.
+void criaLista()
+{
+  inicio = NULL;
 }
 
-
-//APENAS UM EXEMPLO DE CRIAÇÃO DE LISTA, PRECISAMOS ADAPTAR PARA NOSSA VERSÃO
-void criarLista(int n)
+int cadastro()
 {
-    struct node *fnNode, *tmp;
-    int num, i;
-    stnode = (struct node *)malloc(sizeof(struct node));
-
-    if(stnode == NULL) //verifica se o fnnode é NULL e se não houver alocação de memória
+  lista *ponteiro;
+  ponteiro =  (lista *) malloc(sizeof(lista));
+    if (ponteiro==NULL)
     {
-        printf(" Impossível alocar memória");
+      printf("Erro de alocacao\n");
+      return 0;
     }
+  printf("Digite a matricula:");
+  scanf("%d",&ponteiro->matricula);
+  printf("Digite o nome:");
+  scanf(" %[^\n]",ponteiro->nome);
+  printf("Digite o curso:");
+  scanf(" %[^\n]",ponteiro->curso);
+  printf("Digite a data de nascimento:");
+  scanf(" %[^\n]",ponteiro->datnasc);
+  ponteiro->prox = NULL;
+    if (inicio==NULL)
+      {
+        inicio = ponteiro;
+      }
+    else
+      {
+     ponteiro->prox = inicio;
+     inicio  = ponteiro;
+     }
+   return 1;
+}
+
+void imprime()
+{
+  lista *ponteirolista;
+  if (inicio == NULL) // Caso a lista nao esteja vazia
+  {
+    printf("funcao-impressao>>lista-null--\n\n");
+    return;
+  }
+  ponteirolista = inicio;
+  while (ponteirolista !=NULL) {
+    printf("Matricula = %d\n",ponteirolista->matricula);
+    printf("Nome = %s\n",ponteirolista->nome);
+    printf("Curso = %s\n",ponteirolista->curso);
+    printf("Data de Nascimento = %s\n",ponteirolista->datnasc);
+    ponteirolista = ponteirolista->prox;
+    getchar();
+    getchar();
+  }
+  printf("funcao-impressao>>fim-da-lista ---\n\n");
+}
+
+void imprimeponterio(lista *ponteirolista)///para imprimir resultado da função busca
+{
+
+  if (ponteirolista == NULL) // Caso a função busca restorne null
+  {
+    printf("Não achou!!");
+    return;
+  }
+    printf("funcao-impressao--ponteiro ---\n\n");
+    printf("Matricula = %d\n",ponteirolista->matricula);
+    printf("Nome = %s\n",ponteirolista->nome);
+    printf("Curso = %s\n",ponteirolista->curso);
+    printf("Data de Nascimento = %s\n",ponteirolista->datnasc);
+    getchar();
+    getchar();
+}
+
+///-------------------------------------------------pesquisa
+///pesquisa por matricula
+lista *pesquisamatricula()
+{
+   int dado;
+   printf("Digite a matricula:");
+   scanf("%d",&dado);
+  lista *ponteiro;
+  if (inicio == NULL)
+  {
+    return NULL;  // Lista Vazia
+  }
+  // Caso a lista nao esteja vazia
+  ponteiro = inicio;
+  while (ponteiro !=NULL) {
+     printf("procurando %d==%d\n",dado,ponteiro->matricula);///comentar parar para de mostra lixo na tela
+     getchar();///comentar ai ñ pede enter
+    if (ponteiro->matricula == dado)  // achou !!
+    {
+       printf("\nACHOU %d==%d!!!\n",ponteiro->matricula,dado);
+
+   return (ponteiro);        // retorna um ponteiro para função imprimir ponteiro
+
+                   }
     else
     {
-// Leitura
 
-        printf(" Input data for node 1 : ");
-        scanf("%d", &num);
-        stnode->num = num;
-        stnode->nextptr = NULL; // links the address field to NULL
-        tmp = stnode;
-// Creating n nodes and adding to linked list
-        for(i=2; i<=n; i++)
-        {
-            fnNode = (struct node *)malloc(sizeof(struct node));
-            if(fnNode == NULL)
-            {
-                printf(" Memory can not be allocated.");
-                break;
-            }
-            else
-            {
-                printf(" Input data for node %d : ", i);
-                scanf(" %d", &num);
+        ponteiro = ponteiro->prox;
 
-                fnNode->num = num;      // links the num field of fnNode with num
-                fnNode->nextptr = NULL; // links the address field of fnNode with NULL
-
-                tmp->nextptr = fnNode; // links previous node i.e. tmp to the fnNode
-                tmp = tmp->nextptr;
-            }
-        }
-    }
-}
-
-node inserir(){
-node *novoNo = NULL;
-
-Aluno aluno_1;
-
-printf("Qual o nome do Aluno que voc� deseja inserir?\n");
-scanf("%c",&aluno_1.Nome);
-
-printf("Qual a idade do Aluno que voc� deseja inserir?\n");
-scanf("%i",&aluno_1.idade);
-
-printf("Qual o telefone do Aluno que voc� deseja inserir?\n");
-scanf("%c",&aluno_1.Telefone);
-
-novoNo = (struct node*) malloc(sizeof(struct node*));
-novoNo->data=aluno_1;
-
-return *novoNo;
-}
-
-//Funcao que vai procurar na lista encadeada
-long search(long *pointer, long n, long find)
-{
-   long c;
-
-   for (c = 0; c < n; c++) {
-      if (*(pointer+c) == find)
-         return c;
-   }
-
-   return -1;
-}
-
-//Funcao que vai listar nossa lista encadeada
-void printKeys(LinkedList *list){
-    item* ptrTemp = list->head;
-    while (ptrTemp->next != NULL) {
-        printf("%d",ptrTemp->data);
-        ptrTemp = ptrTemp->next;
-    }
-    printf("%d\n",ptrTemp->data);
-}
-
-
-//Fun��o respons�vel pela cria��o do Menu
- void CriaMenu(){
-  char tecla;
-    printf("1 - Para Inserir um Aluno;\n");
-    printf("2 - Para Pesquisar um Aluno\n");
-    printf("3 - Para Remover um Aluno;\n");
-    printf("4 - Para Mostrar todos os Alunos;\n");
-    printf("O que deseja fazer?\n");
-    scanf("%c",&tecla);
-  if(tecla = "1"){
-    executar(tecla);
-  }else if(tecla = "2"){
-    executar(tecla);
-  }else if(tecla = "3"){
-    executar(tecla);
-  }else if(tecla = "4"){
-    executar(tecla);
-  }else{
-    exit(0);
+     }
   }
+  return NULL;
+}
+lista *pesquisanome()
+{
+   char pesq[50];
+   printf("Digite o nome:");
+   scanf(" %[^\n]",pesq);
+  lista *ponteiro;
+  if (inicio == NULL)
+  {
+    return NULL;  // Lista Vazia
+  }
+  // Caso a lista nao esteja vazia
+  ponteiro = inicio;
+  while (ponteiro !=NULL) {
+     printf("procurando %s==%s\n",pesq,ponteiro->nome);///comentar parar para de mostra lixo na tela
+     getchar();///comentar ai ñ pede enter
+    if (strcmp (ponteiro->nome,pesq)== 0 )  // achou !!
+    {
+            printf("\nACHOU %s==%s!!!\n",ponteiro->nome,pesq);
+         return (ponteiro);        // retorna um ponteiro para função imprimr
+
+                   }
+    else
+    {
+
+        ponteiro = ponteiro->prox;
+
+     }
+  }
+  return NULL;
+}
+
+///esvazia a fila
+void  libera (lista* l)
+{
+   printf("\nFuncao liberar\n");
+  lista* ponteiro = l;
+  while (ponteiro != NULL)
+      {
+        lista* t = ponteiro->prox;
+        free(ponteiro);
+        ponteiro = t;
+      }
 
 }
-//teste para joao
-//teste para tassia
+
+///remove elemento
+int removedado(int dado)
+{
+  lista *ptr, *antes;
+  if (inicio==NULL)
+  {
+      return 0;  // Lista vazia !!!
+  }
+  else
+  {  // Caso a lista nao esteja vazia
+      ptr = inicio;
+      antes = inicio;
+      while (ptr !=NULL)
+      {
+    if (ptr->matricula == dado) // achou !!
+    {
+      if (ptr == inicio) // se esta removendo o primeiro da lista
+      {
+        inicio = inicio->prox;
+        free(ptr);
+        return 1; // removeu !!
+      }
+      else  // esta removendo do meio da lista
+      {
+        antes->prox = ptr->prox;  // Refaz o encadeamento
+        free(ptr);                // Libera a area do nodo
+        return 1;  // removeu !!
+      }
+    }
+    else  // continua procurando no prox. nodo
+    {
+      antes = ptr;
+      ptr = ptr->prox;
+    }
+      }
+      return 0; // Nao achou !!!
+  }
+}
 
 int main()
 {
-    setlocale(LC_ALL, "Portuguese");
-    CriaMenu();
-    return 0;
+  criaLista();///iniciar a lista
+  int op=1;
+  for(;;) //loop infinito ate condicao de parada 'break'
+    {
+    cadastro();
+    printf("Menu cadastro:\n1_Novo\t0_Sair:");
+    scanf("%d",&op);
+      if(op==0)//caso 0 sair do loop
+      {
+        break;
+      }
+    }
+    printf(">>Dados cadastrados<<\n");
+  imprime();///imprimir os dados cadastrados
+  //-------------
+  voltamenu :
+  printf("Menu:\n1_Pesquisa por Matricula\n2_Pesquisa por Nome\n3_Remover\n4_Imprimi\n0_Sair:\n:");
+  scanf("%d",&op);
+  switch (op)
+            {
+              case 1:
+                    {
+                      imprimeponterio(pesquisamatricula());///pesquisa a matricula e retorna para funcao o ponterio para imprimir
+                      break;
+                    }
+
+              case 2:
+                    {
+                      imprimeponterio(pesquisanome());
+                      break;
+                    }
+              case 3:
+                {
+                   imprime();
+                   removedado(pesquisamatricula()->matricula);///a pesquisa retorna um ponteiro para a funcao remover
+                   printf("Removido... imprimindo lista \n");
+                   imprime();
+                   break;
+                }
+              case 4:
+                    {
+                      imprime();
+                      break;
+                    }
+              case 0:
+                    {
+                 libera (inicio);
+                      return 0;
+                      break;
+                    }
+            }
+
+
+  goto voltamenu; //simplesmente volta ao menu
+  return 0;
 }
